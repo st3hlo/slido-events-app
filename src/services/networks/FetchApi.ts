d82cf5415a9673ca9d02732847eaed94918927a7
st3hlo <stehlo1@gmail.com>
2020-08-18 10:23:32 +0200

    db.json update, moving repo, json server update

--- src/services/networks/FetchApi.ts
+++ src/services/networks/FetchApi.ts
@@ -5,7 +5,7 @@ import { isEmpty } from 'lodash';
 export const fetchApi = <S>(dispatch: Dispatch<Action>, actions: ActionMap, props: RequestInit, endpoint?: string) => {
     (async() => {
         try {
-            const res = await fetch(`https://my-json-server.typicode.com/st3hlo/events-web-interface${endpoint || ''}`, props);
+            const res = await fetch(`https://my-json-server.typicode.com/st3hlo/slido-events-app${endpoint || ''}`, props);
             const data = await res.json();
 
             dispatch({ type: actions.success, payload: { result: !isEmpty(data) ? data : null } });
